7ffdb4e6b706a9222ca327631a828373ec48cc13
MelindaH26 <min.huur@gmail.com>
2024-03-09 12:44:03 +1000

    theming updates

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// SVG animations
-
-const tween1 = KUTE.fromTo(
-    '#blob-front-1',
-    { path: '#blob-front-1' },
-    { path: '#blob-front-2' },
-    { repeat: 999, duration: 6000, yoyo: true}
-);
-
-const tween2 = KUTE.fromTo(
-    '#blob-back-1',
-    { path: '#blob-back-1' },
-    { path: '#blob-back-2' },
-    { repeat: 999, duration: 7800, yoyo: true}
-);
-
-tween1.start();
-tween2.start();
-
-
 // scrolling events
 let lastKnownScrollPosition = 0;
 let ticking = false;
