4819cd354f77883af18c2077ed7366a9700ef533
MelindaH26 <min.huur@gmail.com>
2024-03-12 18:26:36 +1000

    Updated theming in preperation for content

--- script.js
+++ script.js
@@ -3,19 +3,27 @@ let lastKnownScrollPosition = 0;
 let ticking = false;
 let section = 'home'
 
-function doSomething(scrollPos) {
-    console.log(scrollPos);
-    const target = document.getElementById('scroll-text');
-    target.innerHTML = (`portfolio-${section}-${scrollPos}`);
-    ticking = false;
+function updateScrollText(scrollPos) {
+  //Update text in CLI
+  const target = document.getElementById('scroll-text');
+  target.innerHTML = (`portfolio-${section}-${scrollPos}`);
+  ticking = false;
+  // Add or remove class from header
+  var header = document.getElementById("header");
+  if (lastKnownScrollPosition > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
+
 
 document.addEventListener("scroll", (event) => {
   lastKnownScrollPosition = window.scrollY;
 
   if (!ticking) {
     window.requestAnimationFrame(() => {
-      doSomething(lastKnownScrollPosition);
+      updateScrollText(lastKnownScrollPosition);
     });
 
     ticking = true;
